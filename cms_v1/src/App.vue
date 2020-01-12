caea9dea01c2e072c75e26ff1efd8ee82559b5e1
Phạm Tuấn Anh <sp.pta.135@gmail.com>
2020-01-12 21:58:18 +0700

    cms version 1

---
+++ cms_v1/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <compo-header />
+    <compo-menu />
+    <compo-footer />
+  </div>
+</template>
+
+<script>
+import CompoHeader from './components/CompoHeader.vue'
+import CompoMenu from './components/CompoMenu.vue'
+import CompoFooter from './components/CompoFooter.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    CompoHeader,
+    CompoMenu,
+    CompoFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ cms_v1/src/components/CompoFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- FOOTER -->
+  <div id="footer">
+    <p>&copy; binarytheme &nbsp;2020 &nbsp;</p>
+  </div>
+  <!--END FOOTER -->
+</template>
+
+<script>
+export default {
+  name: "compo-footer",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
