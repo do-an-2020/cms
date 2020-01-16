8d875fba7444d251c7ef544a405bfd8c3f7663ea
Hung Đăng Văn <dvhchuot@gmail.com>
2020-01-16 14:23:05 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,25 @@
 <template>
   <div id="app">
-   
-    <compo-header></compo-header>
-    <compo-menu></compo-menu>
-    <compo-content></compo-content>
-    <compo-footer></compo-footer>
+    <component :is="layout">
+        <transition name="fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
+    </component>
   </div>
 </template>
 
-<script language="javascript" type="text/javascript" src='./assets/plugins/jquery-2.0.3.min.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/plugins/bootstrap/js/bootstrap.min.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/plugins/modernizr-2.6.2-respond-1.1.0.min.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/js/for_index.js'></script>
-
-<script language="javascript" type="text/javascript" src='./assets/plugins/jquery-2.0.3.min.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/plugins/bootstrap/js/bootstrap.min.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/plugins/modernizr-2.6.2-respond-1.1.0.min.js'></script>
-
-<script language="javascript" type="text/javascript" src='./assets/plugins/flot/jquery.flot.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/plugins/flot/jquery.flot.resize.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/plugins/flot/jquery.flot.time.js'></script>
-<script language="javascript" type="text/javascript" src='./assets/plugins/flot/jquery.flot.stack.js'></script>
-
 <script>
+  const default_layout = "default";
 
-import CompoHeader from "./layouts/components/CompoHeader";
-import CompoMenu from "./layouts/components/CompoMenu";
-import CompoContent from "./layouts/components/CompoContent";
-import CompoFooter from "./layouts/components/CompoFooter";
-export default {
-  components: {
-    CompoHeader,
-    CompoMenu,
-    CompoContent,
-    CompoFooter
+  export default {
+    computed: {
+      layout() {
+        return (this.$route.meta.layout || default_layout) + '-layout';
+      }
+    }
   }
-};
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  @import "assets/base.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,34 +1,23 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
-import './assets/css/bootstrap-fileupload.min.css'
-import './assets/css/bootstrap-wysihtml5-hack.css'
-import './assets/css/calendar.css'
-import './assets/css/countdown.css'
-import './assets/css/error.css'
-import './assets/css/jqstooltip.css'
-import './assets/css/jquery-ui.css'
-import './assets/css/jquery.cleditor-hack.css'
-import './assets/css/layout2.css'
-import './assets/css/layout2.min.css'
-import './assets/css/login.css'
-import './assets/css/main.css'
 
-import './assets/css/main.min.css'
-import './assets/css/Markdown.Editor.hack.css'
-import './assets/css/MoneAdmin.css'
-import './assets/css/Offline.css'
-import './assets/css/style.css'
-import './assets/css/theme.css'
+import BootstrapVue from "bootstrap-vue"
 
-import './assets/plugins/bootstrap/css/bootstrap.css'
-import './assets/plugins/Font-Awesome/css/font-awesome.css'
-import './assets/plugins/timeline/timeline.css'
-import './assets/plugins/flot/examples/examples.css'
+import App from './App'
 
-Vue.config.productionTip = false
+import Default from './Layout/Wrappers/baseLayout.vue';
+import Pages from './Layout/Wrappers/pagesLayout.vue';
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+
+Vue.component('default-layout', Default);
+Vue.component('userpages-layout', Pages);
 
 new Vue({
+  el: '#app',
   router,
-  render: h => h(App)
-}).$mount('#app')
+  template: '<App/>',
+  components: { App }
+});
