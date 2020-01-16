95ea5bd93759038c978d8efd1101697c4080864f
Phạm Tuấn Anh <sp.pta.135@gmail.com>
2020-01-16 11:39:41 +0700

    commit

---
+++ src/layouts/components/CompoFooter.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- FOOTER -->
+  <div id="footer">
+    <a href="">Hello</a>
+    <p>&copy; binarytheme &nbsp;2020 &nbsp;</p>
+  </div>
+  <!--END FOOTER -->
+</template>
+
+<script>
+export default {
+  name: "compo-footer",
+
+  data() {
+    return {};
+  },
+  beforCreate: function() {},
+  created: function(){},
+  beforeMount: function(){},
+  mounted: function(){},
+  beforeUpdate: function(){},
+  updated: function(){},
+  beforeDestroy: function(){},
+  destroyed: function(){},
+  methods:{
+
+  },
+  computed:{
+
+  },
+  watch:{
+
+  },
+  components:{
+    
+  }
+};
+</script>
+
+<style>
+</style>
